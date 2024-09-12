e170861b3f9d34da6704543fe069388bad131d20
tylersherrick <tyler.sherrick@gmail.com>
2024-09-12 16:24:03 +0000

    have functionality working now to get data showing and carrying over when clicking on one game. will implement this to the main sports-tracker soon

--- script/script.js
+++ script/script.js
@@ -21,28 +21,26 @@ const updateViews = () => {
 const mlbDisplay = () => {
     sportsDiv.innerHTML = `<h1>Todays Sports Scores</h1>`;
     sportsData.forEach(event => {
+        console.log(sportsData);
         const gameId = event.id;
         const awayTeam = event.competitions[0].competitors[1].team.displayName;
         const homeTeam = event.competitions[0].competitors[0].team.displayName;
         const newElement = document.createElement("div");
-        newElement.setAttribute("id", gameId);
-        newElement.setAttribute("class", "game-row");
-        newElement.textContent = `Game ID: ${gameId} ${awayTeam} at ${homeTeam}`;
+        newElement.id = gameId;
+        newElement.innerHTML = `${awayTeam} at ${homeTeam}`;
         sportsDiv.append(newElement);
-    });
 
-    document.querySelectorAll('.game-row').forEach(element => {
-        element.addEventListener('click', (event) => {
-            const clickedElement = event.target;
-            const gameId = clickedElement.id;
-            const gameName = clickedElement.textContent;            
-            sportsDiv.innerHTML = `<button id="back-button">Back</button>`;
-            sportsDiv.innerHTML += `<p>${gameName}</p>`;
-            document.getElementById('back-button').addEventListener('click', () => {
-                mlbDisplay();
-            });
+        newElement.addEventListener('click', (e) => {
+            console.log(e.target.id + ` ${awayTeam} at ${homeTeam}`); 
+            sportsDiv.innerHTML = `
+                <button id="all-content">Back</button>
+                <p>${e.target.id}: ${awayTeam} at ${homeTeam}</p>
+            `;
+            const backButton = document.getElementById("all-content");
+        backButton.addEventListener('click', mlbDisplay)
         });
     });
-}
+};
+
 
 fetchData();
