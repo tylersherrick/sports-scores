3d8561aa52a5df839ed2c9fea14a697619df3640
tylersherrick <tyler.sherrick@gmail.com>
2024-09-19 22:19:06 +0000

    added score will add all the logic and game info

--- script/script.js
+++ script/script.js
@@ -25,16 +25,29 @@ const mlbDisplay = () => {
         const gameId = event.id;
         const awayTeam = event.competitions[0].competitors[1].team.displayName;
         const homeTeam = event.competitions[0].competitors[0].team.displayName;
+        const awayScore = event.competitions[0].competitors[1].score;
+        const homeScore = event.competitions[0].competitors[0].score;
+        const inning = event.status.type.detail;
+        const gameStatus = event.status.type.shortDetail;
+        const outs = event.competitions[0].outsText;
+        const situation = event.competitions[0].situation;
+        const balls = situation ? situation.balls : 'N/A';
+        const strikes = situation ? situation.strikes : 'N/A';
+        const isFirst = situation ? situation.onFirst : 'N/A';
+        const isSecond = situation ? situation.onSecond : 'N/A';
+        const isThird = situation ? situation.onThird : 'N/A';
         const newElement = document.createElement("div");
         newElement.id = gameId;
-        newElement.innerHTML = `${awayTeam} at ${homeTeam}`;
+        newElement.innerHTML += `${awayTeam} ${awayScore} at ${homeTeam} ${homeScore}`;
         sportsDiv.append(newElement);
 
         newElement.addEventListener('click', (e) => {
             console.log(e.target.id + ` ${awayTeam} at ${homeTeam}`); 
             sportsDiv.innerHTML = `
                 <button id="all-content">Back</button>
-                <p>${e.target.id}: ${awayTeam} at ${homeTeam}</p>
+                <p>${e.target.id}: </p>
+                <p>${awayTeam} - ${awayScore}</p>
+                <p>${homeTeam} - ${awayScore}</p>
             `;
             const backButton = document.getElementById("all-content");
         backButton.addEventListener('click', mlbDisplay)
